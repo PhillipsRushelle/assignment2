1cfe9094c8c2928c1a68061ddc64db5898147b1f
PhillipsRushelle <113263611+PhillipsRushelle@users.noreply.github.com>
2022-12-13 21:45:08 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,5 +3,10 @@ Name: Rushelle Phillips
 Student Number: 300508161
 Date: Oct 22th, 2022 */
 
-export const Secret = "MySecret";
-export const MongoURI  = "mongodb+srv://andreacnas:tapioca123@cluster0.ibkzrfp.mongodb.net/?retryWrites=true&w=majority";
+module.exports =
+{
+    //"URI" : "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://7T4YRcnjmMcuRd9H:7T4YRcnjmMcuRd9H@cluster0.vbzzaa0.mongodb.net/Contact",
+    "Secret": 'SomeSecret'
+}
+
